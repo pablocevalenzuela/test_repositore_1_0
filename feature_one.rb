886e8b1f28ad282b71d2f996e89eae989efc307f
Pablo Valenzuela <pabloce.valenzuela@gmail.com>
2024-03-05 16:14:03 -0300

    new changes

---
+++ feature_one.rb
@@ -0,0 +1,1 @@
+puts "this's a new feature."
--- main.rb
+++ main.rb
@@ -1,16 +1,1 @@
-# Calculate the factorial of a number
-def factorial(n)
-    if n == 0 || n == 1
-      return 1
-    else
-      return n * factorial(n - 1)
-    end
-  end
-  
-  # Calculate the number of permutations of n objects taken r at a time
-  def permutations(n, r)
-    return factorial(n) / factorial(n - r)
-  end
-  
-  # Test the permutations function
-  puts permutations(5, 3)  # Output: 60
+puts "Hello human friend."
